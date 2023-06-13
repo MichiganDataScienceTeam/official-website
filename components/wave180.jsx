3499ce867e99e437ef989384b4c13d1381be125b
jmshaver <jmshaver@umich.edu>
2023-06-13 18:44:38 -0500

    added projects, ad and sponsor sections on main page

---
+++ components/wave180.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function Wave() {
+  return React.createElement(
+    'svg',
+    {
+      preserveAspectRatio: 'none',
+      xmlns: 'http://www.w3.org/2000/svg',
+      x: '0px',
+      y: '0px',
+      viewBox: '0 0 1920 100.1',
+      style: {
+        height: '7rem',
+        width: '100%',
+        transform: 'rotate(180deg)',
+      },
+    },
+    React.createElement('path', {
+      fill: '#212121',
+      d: 'M0,0c0,0,934.4,93.4,1920,0v100.1H0L0,0z',
+    })
+  );
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,6 @@
 import Layout from '@/components/layout';
 import Wave from '@/components/wave';
+import Wave180 from '@/components/wave180';
 import Link from 'next/link';
 import fs from 'fs';
 import path from 'path';
@@ -9,61 +10,154 @@ import Image from 'next/image';
 export default function Home({ content }) {
   return (
     <Layout>
-      <div className='text-center hero'>
+      <div className="text-center hero">
         <div className="container mx-auto ">
-          <div className='flex gap-8 items-center'>
-            <div className='w-1/2'>
-            <h1 className='mb-5 text-5xl font-bold'>U of M's Premier Data Science Club</h1>
-          <p className='mb-10 text-2xl'>
-          We empower the next generation of data scientists at the University of Michigan through education and exploration.
-          </p>
+          <div className="flex gap-8 items-center md:flex-row flex-col-reverse ">
+            <div className="md:w-1/2 w-full">
+              <h1 className="mb-2 md:mb-5 lg:text-5xl md:text-3xl text-xl font-bold">
+                U of M's Premier Data Science Club
+              </h1>
+              <p className="mb-10 lg:text-2xl md:text-xl text-lg">
+                We empower the next generation of data scientists at
+                the University of Michigan through education and
+                exploration.
+              </p>
             </div>
-            <Image className='w-1/2' width='500' height='500' src="/images/Dataframe_Graphic.svg" alt="Image of a dataframe" />
+            <Image
+              className="md:w-1/2 w-full"
+              width="500"
+              height="500"
+              src="/images/Dataframe_Graphic.svg"
+              alt="Image of a dataframe"
+            />
           </div>
-          
-          <div className='flex justify-center gap-5'>
+
+          <div className="flex justify-center gap-5">
             <Link
-              className='border-2 rounded-full p-4 hover:-translate-y-1'
-              href=''
+              className="border-2 rounded-full p-3 hover:-translate-y-1"
+              href=""
             >
               Join Us
             </Link>
             <Link
-              className='border-2 rounded-full p-4 hover:-translate-y-1'
-              href=''
+              className="border-2 rounded-full p-3 hover:-translate-y-1"
+              href=""
             >
               Work With Us
             </Link>
           </div>
         </div>
-        
+
         <Wave></Wave>
       </div>
-    <div className="container mx-auto">
-    <div className="flex items-center text-center gap-8">
-      <div className='w-1/3'>
-      <p>Join our community of data science
-enthusiasts with</p>
-      <p className='text-3xl'>200+ Members</p>
-      <p> across 10+ different majors in 8
-different U of M Schools </p>
+      <div className="container mx-auto">
+        <div className="grid bullets  items-center text-center gap-x-8 gap-y-2 grid-flow-col">
+          <p className="self-end">
+            Join our community of data science enthusiasts with
+          </p>
+          <p className="text-3xl">200+ Members</p>
+          <p className="self-start">
+            across 10+ different majors in 8 different U of M Schools
+          </p>
+
+          <p className="self-end">
+            Build your data science expertise by working on one of our
+          </p>
+          <p className="text-3xl">8-10 Projects</p>
+          <p className="self-start">
+            each semester in domains like finance, sports, computer
+            vision, and more
+          </p>
+          <p className="self-end">
+            Invest in your future career through
+          </p>
+          <p className="text-3xl">Tons of Events</p>
+          <p className="self-start">
+            in career development, interview prep, education, and
+            networking.
+          </p>
+        </div>
       </div>
-      <div className='w-1/3'>
-      <p>Build your data science expertise 
-by working on one of our</p>
-      <p className='text-3xl'>8-10 Projects</p>
-      <p>
- each semester in domains like 
-finance, sports, computer vision, and more</p>
-      </div><div className='w-1/3'>
-      <p>Invest in your future career through</p>
-      <p className='text-3xl'>Tons of Events</p>
-      <p>in career development, interview prep,
-education, and networking. </p>
+      <div className="bg-grey">
+        <Wave180 className="rotate-180"></Wave180>
+        <div className="container mx-auto py-4">
+          <h2 className="text-3xl text-center bold">
+            Recent Projects
+          </h2>
+          <div className="flex gap-4 flex-col sm:flex-row p-4">
+            <div className="project-card w-full">
+              <Image
+                className="w-full"
+                width="500"
+                height="500"
+                src="/images/temp.png"
+                alt="temp"
+              />
+              <h3 className="text-lg">Image Colorization</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing
+                elit. Aliquam
+              </p>
+            </div>
+            <div className="project-card w-full">
+              <Image
+                className="w-full"
+                width="500"
+                height="500"
+                src="/images/temp.png"
+                alt="temp"
+              />
+              <h3 className="text-lg">Movie Quotes</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing
+                elit. Aliquam pariatur reiciendis
+              </p>
+            </div>
+            <div className="project-card w-full">
+              <Image
+                className="w-full"
+                width="500"
+                height="500"
+                src="/images/temp.png"
+                alt="temp"
+              />
+              <h3 className="text-lg">Wild fires</h3>
+              <p>Lorem ipsum dolor</p>
+            </div>
+          </div>
+        </div>
+        <Wave></Wave>
       </div>
-</div>
-    </div>
-      
+      <div className="container mx-auto py-4">
+        <h2 className="text-3xl text-center">
+          Interested? <br />
+          All UM Ann Arbor students can join for free!
+        </h2>
+        <div className="flex mt-4 justify-center">
+          <Link
+            className="border-2 rounded-full p-3 hover:-translate-y-1"
+            href=""
+          >
+            Join Us
+          </Link>
+        </div>
+      </div>
+      <div className="bg-grey py-4">
+        <div className="container mx-auto">
+          <h2 className="text-center text-xl">
+            MDST is made possible by our sponsors
+          </h2>
+          <div className="flex mt-4 justify-center">
+            <Image
+              className=""
+              width="200"
+              height="200"
+              src="/images/esg.png"
+              alt="temp"
+            />
+          </div>
+        </div>
+      </div>
       {/* <div className='dark:bg-blue-800 p-4'>
         <Markdown className='markdown'>{content}</Markdown>
       </div> */}
@@ -72,7 +166,11 @@ education, and networking. </p>
 }
 
 export async function getStaticProps() {
-  const filePath = path.join(process.cwd(), 'markdown', 'homepage.md');
+  const filePath = path.join(
+    process.cwd(),
+    'markdown',
+    'homepage.md'
+  );
   const fileContent = fs.readFileSync(filePath, 'utf-8');
   const content = fileContent;
   return { props: { content } };
