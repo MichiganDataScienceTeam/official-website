4c1483e9945b5f40dbed50de1207c4568af0cf4f
skunichetty <skunich@umich.edu>
2023-06-22 21:35:59 -0500

    Added locally hosted google font file

---
+++ shared/fonts.js
@@ -0,0 +1,13 @@
+// Font definitions
+import { Inter, DM_Sans } from "next/font/google";
+
+export const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+export const dmSans = DM_Sans({
+  subsets: ["latin"],
+  variable: "--font-dm-sans",
+  weight: "500",
+});
