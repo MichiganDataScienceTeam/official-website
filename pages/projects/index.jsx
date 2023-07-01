d74073a286fe9e15c7c0095b7c49587f597aa645
jmshaver <jmshaver@umich.edu>
2023-07-01 13:36:07 -0500

    rounding changes

--- pages/projects/index.jsx
+++ pages/projects/index.jsx
@@ -1,11 +1,11 @@
-import Layout from '@/components/layout';
-import Wave from '@/components/wave';
-import Wave180 from '@/components/wave180';
-import Link from 'next/link';
-import fs from 'fs';
-import path from 'path';
-import Markdown from 'markdown-to-jsx';
-import Image from 'next/image';
+import Layout from "@/components/layout";
+import Wave from "@/components/wave";
+import Wave180 from "@/components/wave180";
+import Link from "next/link";
+import fs from "fs";
+import path from "path";
+import Markdown from "markdown-to-jsx";
+import Image from "next/image";
 
 export default function Projects({ groupedLinks }) {
   return (
@@ -24,7 +24,7 @@ export default function Projects({ groupedLinks }) {
         {Object.entries(groupedLinks).map(([subdirectory, links]) => (
           <div
             key={subdirectory}
-            className="rounded border-grey border-2 my-2"
+            className="rounded-lg border-grey border-2 my-2"
           >
             <h2 className="p-4 bg-grey">{subdirectory}</h2>
             <ul className="p-4">
@@ -44,11 +44,7 @@ export default function Projects({ groupedLinks }) {
 }
 
 export async function getStaticProps() {
-  const projectsDirectory = path.join(
-    process.cwd(),
-    'public',
-    'projects'
-  );
+  const projectsDirectory = path.join(process.cwd(), "public", "projects");
   const subdirectories = fs
     .readdirSync(projectsDirectory, { withFileTypes: true })
     .filter((dirent) => dirent.isDirectory())
@@ -56,29 +52,23 @@ export async function getStaticProps() {
 
   const groupedLinks = {};
   subdirectories.forEach((subdirectory) => {
-    const subdirectoryPath = path.join(
-      projectsDirectory,
-      subdirectory
-    );
+    const subdirectoryPath = path.join(projectsDirectory, subdirectory);
     const files = fs.readdirSync(subdirectoryPath);
 
     const links = [];
     files.forEach((file) => {
-      if (file.endsWith('.pdf') || file.endsWith('.md')) {
+      if (file.endsWith(".pdf") || file.endsWith(".md")) {
         const label = file
-          .replace('.pdf', '')
-          .replace('.md', '')
-          .split('_')
-          .join(' ');
-        const href = `/projects/${subdirectory}/${file.replace(
-          '.md',
-          ''
-        )}`;
+          .replace(".pdf", "")
+          .replace(".md", "")
+          .split("_")
+          .join(" ");
+        const href = `/projects/${subdirectory}/${file.replace(".md", "")}`;
 
         links.push({ label, href });
       }
     });
-    groupedLinks[subdirectory.split('_').join(' ')] = links;
+    groupedLinks[subdirectory.split("_").join(" ")] = links;
   });
   return {
     props: {
