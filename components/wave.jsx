e51203ffc5acd8b27a8a221d5304313b19187128
skunichetty <skunich@umich.edu>
2023-07-16 17:26:28 -0500

    Reduced amount of curve in wave

--- components/wave.jsx
+++ components/wave.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import React from "react";
 
 export default function Wave() {
   return React.createElement(
-    'svg',
+    "svg",
     {
-      preserveAspectRatio: 'none',
-      xmlns: 'http://www.w3.org/2000/svg',
-      x: '0px',
-      y: '0px',
-      viewBox: '0 0 1920 100.1',
-      style: { height: '7rem', width: '100%' },
+      preserveAspectRatio: "none",
+      xmlns: "http://www.w3.org/2000/svg",
+      x: "0px",
+      y: "0px",
+      viewBox: "0 0 1920 100.1",
+      style: { height: "7rem", width: "100%" },
     },
-    React.createElement('path', {
-      fill: '#212121',
-      d: 'M0,0c0,0,934.4,93.4,1920,0v100.1H0L0,0z',
+    React.createElement("path", {
+      fill: "#212121",
+      d: "M0,0c0,0,934.4,33.4,1920,0v100.1H0L0,0z",
     })
   );
 }
