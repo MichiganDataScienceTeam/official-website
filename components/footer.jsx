1562abfdc7e146a6bf866e06adf894fc38c32f5e
skunichetty <skunich@umich.edu>
2023-08-14 19:03:24 -0400

    Added new MDST logo

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,16 +1,19 @@
 import Link from "next/link";
-import Image from "next/image";
 import Icon from "./icon";
+import Logo from "@/components/logo";
 
 export default function Footer() {
   return (
     <footer className="p-4 flex items-center justify-center gap-2">
-      <Link href="/">Logo</Link>
-
+      <Logo size={36} />
+      <div className="pr-7 leading-tight font-semibold text-sm">
+        Michigan Data <br />
+        Science Team
+      </div>
       <Link href="https://github.com/MichiganDataScienceTeam">
         <Icon name="github" className="text-4xl" />
       </Link>
-      <Link href="https://instagram.com/your-instagram">
+      <Link href="https://instagram.com/michigandatascienceteam">
         <Icon name="instagram" className="text-4xl" />
       </Link>
       <Link href="https://linkedin.com/your-linkedin">
