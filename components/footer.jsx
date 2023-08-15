e979e6ae9bac59860659a9f487c60f09d07d8908
skunichetty <skunich@umich.edu>
2023-08-15 13:09:21 -0400

    Fixed socials links

--- components/footer.jsx
+++ components/footer.jsx
@@ -13,10 +13,10 @@ export default function Footer() {
       <Link href="https://github.com/MichiganDataScienceTeam">
         <Icon name="github" className="text-4xl" />
       </Link>
-      <Link href="https://instagram.com/michigandatascienceteam">
+      <Link href="https://instagram.com/mdst_um">
         <Icon name="instagram" className="text-4xl" />
       </Link>
-      <Link href="https://linkedin.com/your-linkedin">
+      <Link href="https://www.linkedin.com/school/michigan-data-science-team/">
         <Icon name="linkedin" className="text-4xl" />
       </Link>
     </footer>
