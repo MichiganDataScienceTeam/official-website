1f27a43f6472bf9645b0d2d8b18c03744e37975d
Jmshaver <71985860+Jmshaver@users.noreply.github.com>
2023-08-16 18:41:47 -0500

    No scroll on timeline as it isn't as accessible

--- pages/join.jsx
+++ pages/join.jsx
@@ -9,10 +9,13 @@ export default function Join({ content, timeline }) {
     <Layout>
       <Hero title="Joining MDST" />
       <div className="container mx-auto px-2 sm:px-7 pb-24 justify-center grid md:grid-flow-col grid-flow-row gap-8">
-        <div className="bg-grey p-4 sm:p-8 rounded-lg md:max-h-screen max-h-96 overflow-auto">
-          <h2 className="text-3xl font-bold mb-4">{timeline.title}</h2>
-          <Timeline events={timeline.events} />
+        <div>
+          <div className="bg-grey p-4 sm:p-8 rounded-lg">
+            <h2 className="text-3xl font-bold mb-4">{timeline.title}</h2>
+            <Timeline events={timeline.events} />
+          </div>
         </div>
+
         <div className="container mx-auto px-2">
           <Markdown className="markdown font-light">{content}</Markdown>
         </div>
