1bc77ceeada795de3a4934ca378d1473d5278d0d
Jmshaver <71985860+Jmshaver@users.noreply.github.com>
2023-08-17 12:34:22 -0500

    changed class to className

--- components/timeline.jsx
+++ components/timeline.jsx
@@ -1,6 +1,6 @@
 export default function Timeline({ events }) {
   return (
-    <ol class="relative border-l border-grey-xlight">
+    <ol className="relative border-l border-grey-xlight">
       {events.map((event, index) => (
         <li className="mb-4 md:mb-6 ml-4" key={index}>
           <div className="absolute w-3 h-3 bg-grey-xlight rounded-full mt-1.5 -left-1.5"></div>
