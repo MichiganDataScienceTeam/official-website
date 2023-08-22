ad47d271192def71308bc4f98e997edca94d530e
Jmshaver <71985860+Jmshaver@users.noreply.github.com>
2023-08-22 11:59:13 -0500

    google analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,17 @@
 import "@/styles/globals.css";
-
+import Script from "next/script";
 export default function App({ Component, pageProps }) {
   return (
     <div>
       <Component {...pageProps} />
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-JYRZVN797J"></Script>
+      <Script>
+        {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-JYRZVN797J');`}
+      </Script>
     </div>
   );
 }
