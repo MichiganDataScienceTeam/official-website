7daf2bd1f7f3d13fa8592a2d63094fc727abd090
Jmshaver <71985860+Jmshaver@users.noreply.github.com>
2023-08-22 09:37:51 -0500

    added a bunch of meta tags

--- components/headContent.jsx
+++ components/headContent.jsx
@@ -5,6 +5,20 @@ export default function HeadContent({ title, description }) {
         <Head>
             <title>{title}</title>
             <meta name="description" content={description} />
+            <link rel="icon" type="image/png" href="/images/favicon.png" />
+
+            <meta property="og:title" content={title} />
+            <meta property="og:description" content={description} />
+            <meta property="og:image" content="/images/favicon.png" />
+            {/* <meta property="og:url" content="URL of your page" /> */}
+            <meta property="og:type" content="website" />
+
+            <meta name="twitter:title" content={title} />
+            <meta name="twitter:description" content={description} />
+            <meta name="twitter:image" content="/images/favicon.png" />
+            <meta name="twitter:card" content="summary" />
+
+            <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon.png" />
         </Head>
     )
 }
