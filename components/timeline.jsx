f6d93fa6236ef2acc94a23a042a26a40717eaa24
skunichetty <skunich@umich.edu>
2023-08-24 13:38:12 -0400

    Added link for event gcal to recruiting timeline

--- components/timeline.jsx
+++ components/timeline.jsx
@@ -1,20 +1,31 @@
 export default function Timeline({ events }) {
   return (
-    <ol className="relative border-l border-grey-xlight">
-      {events.map((event, index) => (
-        <li className="mb-4 md:mb-6 ml-4" key={index}>
-          <div className="absolute w-3 h-3 bg-grey-xlight rounded-full mt-1.5 -left-1.5"></div>
-          <time className="mb-1 text-sm font-normal leading-none text-grey-xlight">
-            {event.time}
-          </time>
-          <h3 className="text-lg font-semibold text-white snap-center">
-            {event.title}
-          </h3>
-          <p className="mb-4 text-base font-normal text-white">
-            {event.description}
-          </p>
-        </li>
-      ))}
-    </ol>
+    <div>
+      <ol className="relative border-l border-grey-xlight">
+        {events.map((event, index) => (
+          <li className="mb-4 md:mb-6 ml-4" key={index}>
+            <div className="absolute w-3 h-3 bg-grey-xlight rounded-full mt-1.5 -left-1.5"></div>
+            <time className="mb-1 text-sm font-normal leading-none text-grey-xlight">
+              {event.time}
+            </time>
+            <h3 className="text-lg font-semibold text-white snap-center">
+              {event.title}
+            </h3>
+            <p className="mb-4 text-base font-normal text-white">
+              {event.description}
+            </p>
+          </li>
+        ))}
+      </ol>
+      <div>
+        Subscribe to our events calendar{" "}
+        <a
+          className="font-bold hover:underline"
+          href="https://calendar.google.com/calendar/embed?src=c_22ca0c151585760442cad5796fb91bd18b7db11d813e9143e38549aadce65afe%40group.calendar.google.com&ctz=America%2FNew_York"
+        >
+          here
+        </a>
+      </div>
+    </div>
   );
 }
