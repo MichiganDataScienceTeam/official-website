3bbb7d746c318059fc60956620d4e3725f35e889
Sachchit Kunichetty <71106771+skunichetty@users.noreply.github.com>
2023-08-26 13:29:13 -0400

    Updated basepath + updated links (#26)
    * Removed special github testing
    
    * Updatd links for website

--- next.config.js
+++ next.config.js
@@ -3,12 +3,12 @@ const isGithubActions = process.env.GITHUB_ACTIONS || false;
 let assetPrefix = "";
 let basePath = "";
 
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+// if (isGithubActions) {
+//   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
 
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
+//   assetPrefix = `/${repo}/`;
+//   basePath = `/${repo}`;
+// }
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
