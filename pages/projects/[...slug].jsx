d4c43f5bd77ba49ef298d91ac15ad11fd1f87e57
igiallan <igiallan@umich.edu>
2024-07-10 09:58:05 -0400

    Projects layout update. Automatically sorts from most to least recent semester and displays accordingly. To add images, put them in public/images/projects/{ lowercase_project_name }. Will fix link display later.

--- pages/projects/[...slug].jsx
+++ pages/projects/[...slug].jsx
@@ -3,109 +3,61 @@ import fs from "fs";
 import path from "path";
 import Markdown from "markdown-to-jsx";
 import Layout from "@/components/layout";
-import Wave from "@/components/wave";
 import Hero from "@/components/hero";
 import Image from "next/image";
 import HeadContent from "@/components/headContent";
-function ProjectPage({ content, title, route }) {
 
+function ProjectPage({ content, title, images }) {
   const router = useRouter();
   const basePath = router.basePath;
 
-  const dir = basePath
-    ? `${basePath}/${route}`
-    : `/${route}`
-
   return (
     <Layout>
-      <HeadContent title={title} description={title + "is a project done with The Michigan Data Science Team - MDST, the largest data science club at the University of Michigan."} />
-
+      <HeadContent 
+        title={title} 
+        description={`${title} is a project done with The Michigan Data Science Team - MDST, the largest data science club at the University of Michigan.`} 
+      />
       <Hero title={title} />
       <div className="container mx-auto px-2">
-        <Markdown className="markdown" options={{
-          overrides: {
-            img: {
-              component: renderImage,
-              props: {
-                dir: dir,
-              },
-            },
-          },
-        }}>{content}</Markdown> :
+        <Markdown className="markdown">{content}</Markdown>
+        {images.map((img, idx) => (
+          <Image key={idx} src={img} width={500} height={500} alt={title} />
+        ))}
       </div>
     </Layout>
   );
 }
 
-const renderImage = (props) => {
-  // Modify the 'src' attribute as needed
-  const customSrc = props.dir + "/" + props.src;
-
-  return <Image {...props} src={customSrc} width={500} height={500} />;
-};
-
 export async function getStaticProps({ params }) {
   const [subdirectory, innerDir] = params.slug;
-  const filePath = path.join(
-    process.cwd(),
-    'public',
-    'projects',
-    subdirectory,
-    innerDir,
-    `writeup.md`
-  );
-
-
+  const filePath = path.join(process.cwd(), 'public', 'projects', subdirectory, innerDir, 'writeup.md');
   const fileContent = fs.readFileSync(filePath, 'utf-8');
+  
+  const imagesDir = path.join(process.cwd(), 'public', 'projects', subdirectory, innerDir, 'images');
+  const images = fs.existsSync(imagesDir) ? fs.readdirSync(imagesDir).map(img => path.join('/projects', subdirectory, innerDir, 'images', img)) : [];
 
   return {
     props: {
       content: fileContent,
       title: innerDir.split("_").join(" "),
-      route: path.join('projects', subdirectory,
-        innerDir),
-      md: true,
+      images,
     },
   };
 }
 
 export async function getStaticPaths() {
   const projectsDirectory = path.join(process.cwd(), "public", "projects");
-  const subdirectories = fs
-    .readdirSync(projectsDirectory, { withFileTypes: true })
-    .filter((dirent) => dirent.isDirectory())
-    .map((dirent) => ({
-      name: dirent.name,
-      createdAt: fs.statSync(path.join(projectsDirectory, dirent.name)).birthtime,
-    }))
-    .sort((a, b) => b.createdAt - a.createdAt)
+  const subdirectories = fs.readdirSync(projectsDirectory, { withFileTypes: true }).filter(dirent => dirent.isDirectory());
 
-  const paths = [];
-
-  subdirectories.forEach((subdirectory) => {
-    const subdirectoryPath = path.join(projectsDirectory, subdirectory.name);
-    const innerDirectories = fs
-      .readdirSync(subdirectoryPath, { withFileTypes: true })
-      .filter((dirent) => dirent.isDirectory())
-      .map((dirent) => dirent.name);
-
-    innerDirectories.forEach((innerDir) => {
-      const innerDirPath = path.join(subdirectoryPath, innerDir);
-
-      const writeupMdPath = path.join(innerDirPath, "writeup.md");
-
-      if (fs.existsSync(writeupMdPath)) {
-        paths.push({ params: { slug: [subdirectory.name, innerDir] } });
-      }
-    });
+  const paths = subdirectories.flatMap(subdirectory => {
+    const innerDirectories = fs.readdirSync(path.join(projectsDirectory, subdirectory.name), { withFileTypes: true }).filter(dirent => dirent.isDirectory());
+    return innerDirectories.map(innerDir => ({ params: { slug: [subdirectory.name, innerDir.name] } }));
   });
 
   return {
     paths,
-    // paths: [{ params: { slug: ["Winter_2023", "Wildfire_Spread_Prediction"] } }],
-    fallback: false, // Set to false to return a 404 if the page doesn't exist
+    fallback: false,
   };
 }
-
 
 export default ProjectPage;
--- pages/projects/index.jsx
+++ pages/projects/index.jsx
@@ -8,21 +8,18 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import Icon from "@/components/icon";
 
-
 export default function Projects({ groupedLinks }) {
   const router = useRouter();
   const basePath = router.basePath;
 
   return (
     <Layout>
-      <HeadContent
-        title={"Michigan Data Science Team - Projects"}
-        description={
-          "Michigan Data Science Team - MDST is the largest data science club at the University of Michigan. Here are some of our past projects."
-        }
+      <HeadContent 
+        title={"Michigan Data Science Team - Projects"} 
+        description={"Michigan Data Science Team - MDST is the largest data science club at the University of Michigan. Here are some of our past projects."} 
       />
       <Hero title="Our Past Projects" />
-      {Object.entries(groupedLinks).map(([subdirectory, links], index) => (
+      {Object.entries(groupedLinks).map(([subdirectory, links]) => (
         <ProjectSection key={subdirectory} subdirectory={subdirectory} links={links} basePath={basePath} />
       ))}
     </Layout>
@@ -45,13 +42,14 @@ function ProjectSection({ subdirectory, links, basePath }) {
 }
 
 function ProjectCard({ project, basePath }) {
+  const imagePath = `${basePath}/images/projects/${project.label.toLowerCase().split(" ").join("_")}.jpg`;
   return (
     <div className="text-left sm:text-center rounded bg-grey py-2 sm:py-4 px-2 sm:px-8 w-full sm:w-60 flex sm:block gap-8">
       <Image
         className="sm:mx-auto sm:mb-4 sm:w-44 sm:h-44 w-24 h-24 my-auto rounded-full object-cover"
         width="176"
         height="176"
-        src={`${basePath}/images/projects/${project.label.toLowerCase().split(" ").join("_")}.jpg`}
+        src={imagePath}
         alt={project.label}
       />
       <div className="">
@@ -93,50 +91,31 @@ function ProjectCard({ project, basePath }) {
 
 export async function getStaticProps() {
   const projectsDirectory = path.join(process.cwd(), "public", "projects");
-  const subdirectories = fs
-    .readdirSync(projectsDirectory, { withFileTypes: true })
-    .filter((dirent) => dirent.isDirectory())
-    .map((dirent) => ({
-      name: dirent.name,
-    }))
-    .sort((a, b) => {
-      const order = ["Fall_", "Winter_"];
+  const subdirectories = fs.readdirSync(projectsDirectory, { withFileTypes: true }).filter(dirent => dirent.isDirectory());
 
-      const getOrderIndex = (dirName) => {
-        const prefix = order.find((prefix) => dirName.startsWith(prefix));
-        return prefix ? 0 : 1;
-      };
+  const sortedSubdirectories = subdirectories.sort((a, b) => {
+    const [aSeason, aYear] = a.name.split('_');
+    const [bSeason, bYear] = b.name.split('_');
+    const seasonOrder = { Winter: 0, Fall: 1 };
 
-      const orderComparison = getOrderIndex(a.name) - getOrderIndex(b.name);
+    if (aYear === bYear) {
+      return seasonOrder[aSeason] - seasonOrder[bSeason];
+    } else {
+      return parseInt(bYear) - parseInt(aYear);
+    }
+  });
 
-      if (orderComparison !== 0) {
-        return orderComparison;
-      } else if (getOrderIndex(a.name) === 0) {
-        const aNumber = parseInt(a.name.split("_")[1]);
-        const bNumber = parseInt(b.name.split("_")[1]);
-        return bNumber - aNumber;
-      } else {
-        return a.name.localeCompare(b.name);
-      }
-    });
-
-  const groupedLinks = {};
-  subdirectories.forEach((subdirectory) => {
+  const groupedLinks = sortedSubdirectories.reduce((acc, subdirectory) => {
     const subdirectoryPath = path.join(projectsDirectory, subdirectory.name);
-    const innerDirectories = fs
-      .readdirSync(subdirectoryPath, { withFileTypes: true })
-      .filter((dirent) => dirent.isDirectory())
-      .map((dirent) => dirent.name);
+    const innerDirectories = fs.readdirSync(subdirectoryPath, { withFileTypes: true }).filter(dirent => dirent.isDirectory());
 
-    const links = [];
-
-    innerDirectories.forEach((innerDir) => {
-      const innerDirPath = path.join(subdirectoryPath, innerDir);
+    const links = innerDirectories.map(innerDir => {
+      const innerDirPath = path.join(subdirectoryPath, innerDir.name);
       const writeupMdPath = path.join(innerDirPath, "writeup.md");
       const writeupPdfPath = path.join(innerDirPath, "writeup.pdf");
       const linkTxtPath = path.join(innerDirPath, "link.txt");
 
-      const label = innerDir.split("_").join(" ");
+      const label = innerDir.name.split("_").join(" ");
       let type, href;
 
       if (fs.existsSync(linkTxtPath)) {
@@ -149,22 +128,23 @@ export async function getStaticProps() {
           type = "link";
         }
       } else if (fs.existsSync(writeupMdPath)) {
-        href = `/projects/${subdirectory.name}/${innerDir}`;
+        href = `/projects/${subdirectory.name}/${innerDir.name}`;
         type = "md";
       } else if (fs.existsSync(writeupPdfPath)) {
-        href = `/projects/${subdirectory.name}/${innerDir}/writeup.pdf`;
+        href = `/projects/${subdirectory.name}/${innerDir.name}/writeup.pdf`;
         type = "pdf";
       } else {
         href = "";
         type = "none";
       }
-      links.push({ label, href, type });
+      return { label, href, type };
     });
 
     if (links.length > 0) {
-      groupedLinks[subdirectory.name.split("_").join(" ")] = links;
+      acc[subdirectory.name.split("_").join(" ")] = links;
     }
-  });
+    return acc;
+  }, {});
 
   return {
     props: {
